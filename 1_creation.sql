e47ec0b0d4303c8b645f6c8943fb59bd118654d8
Gab <119743712+Codercestcool@users.noreply.github.com>
2026-03-14 17:50:19 +0100

    Update 1_creation.sql

--- 1_creation.sql
+++ 1_creation.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE IF NOT EXISTS projet;
-USE projet;
+CREATE DATABASE IF NOT EXISTS projet_durand_guichard;
+USE projet_durand_guichard;
 
 CREATE TABLE Type(
    t_id INT,
@@ -40,9 +40,14 @@ CREATE TABLE Couleur(
    PRIMARY KEY(c_id)
 );
 
+CREATE TABLE Forme(
+   f_id INT,
+   f_nom VARCHAR(50),
+   f_description_forme VARCHAR(50),
+   PRIMARY KEY(f_id)
+);
+
 CREATE TABLE Pokémon(
-   r_id INT,
-   g_num INT,
    p_numero_pokedex INT,
    p_nom_francais VARCHAR(50),
    p_nom_anglais VARCHAR(50),
@@ -53,76 +58,63 @@ CREATE TABLE Pokémon(
    p_fabuleux BOOLEAN,
    p_reproduction BOOLEAN,
    p_description VARCHAR(255),
-   PRIMARY KEY(r_id, g_num, p_numero_pokedex),
+   r_id INT NOT NULL,
+   g_num INT NOT NULL,
+   PRIMARY KEY(p_numero_pokedex),
    FOREIGN KEY(r_id) REFERENCES Région(r_id),
    FOREIGN KEY(g_num) REFERENCES Génération(g_num)
 );
 
-CREATE TABLE Forme(
-   r_id INT,
-   g_num INT,
-   p_numero_pokedex INT,
-   f_id INT,
-   f_nom VARCHAR(50),
-   f_description_forme VARCHAR(50),
-   PRIMARY KEY(r_id, g_num, p_numero_pokedex, f_id),
-   FOREIGN KEY(r_id, g_num, p_numero_pokedex) REFERENCES Pokémon(r_id, g_num, p_numero_pokedex)
-);
-
 CREATE TABLE Etre(
-   r_id INT,
-   g_num INT,
    p_numero_pokedex INT,
    t_id INT,
    t_nom VARCHAR(50),
-   PRIMARY KEY(r_id, g_num, p_numero_pokedex, t_id, t_nom),
-   FOREIGN KEY(r_id, g_num, p_numero_pokedex) REFERENCES Pokémon(r_id, g_num, p_numero_pokedex),
+   PRIMARY KEY(p_numero_pokedex, t_id, t_nom),
+   FOREIGN KEY(p_numero_pokedex) REFERENCES Pokémon(p_numero_pokedex),
    FOREIGN KEY(t_id, t_nom) REFERENCES Type(t_id, t_nom)
 );
 
 CREATE TABLE Avoir_pour(
-   r_id INT,
-   g_num INT,
    p_numero_pokedex INT,
    go_id INT,
-   PRIMARY KEY(r_id, g_num, p_numero_pokedex, go_id),
-   FOREIGN KEY(r_id, g_num, p_numero_pokedex) REFERENCES Pokémon(r_id, g_num, p_numero_pokedex),
+   PRIMARY KEY(p_numero_pokedex, go_id),
+   FOREIGN KEY(p_numero_pokedex) REFERENCES Pokémon(p_numero_pokedex),
    FOREIGN KEY(go_id) REFERENCES Groupe_Oeuf(go_id)
 );
 
 CREATE TABLE Apparaitre_dans(
-   r_id INT,
-   g_num INT,
    p_numero_pokedex INT,
-   g_num_1 INT,
+   g_num INT,
    j_id INT,
-   PRIMARY KEY(r_id, g_num, p_numero_pokedex, g_num_1, j_id),
-   FOREIGN KEY(r_id, g_num, p_numero_pokedex) REFERENCES Pokémon(r_id, g_num, p_numero_pokedex),
-   FOREIGN KEY(g_num_1, j_id) REFERENCES Jeu(g_num, j_id)
+   PRIMARY KEY(p_numero_pokedex, g_num, j_id),
+   FOREIGN KEY(p_numero_pokedex) REFERENCES Pokémon(p_numero_pokedex),
+   FOREIGN KEY(g_num, j_id) REFERENCES Jeu(g_num, j_id)
 );
 
 CREATE TABLE Evoluer(
-   r_id INT,
-   g_num INT,
    p_numero_pokedex INT,
-   r_id_1 INT,
-   g_num_1 INT,
    p_numero_pokedex_1 INT,
    Niveau_minimal VARCHAR(50),
    Condition_spéciale VARCHAR(50),
-   PRIMARY KEY(r_id, g_num, p_numero_pokedex, r_id_1, g_num_1, p_numero_pokedex_1),
-   FOREIGN KEY(r_id, g_num, p_numero_pokedex) REFERENCES Pokémon(r_id, g_num, p_numero_pokedex),
-   FOREIGN KEY(r_id_1, g_num_1, p_numero_pokedex_1) REFERENCES Pokémon(r_id, g_num, p_numero_pokedex)
+   PRIMARY KEY(p_numero_pokedex, p_numero_pokedex_1),
+   FOREIGN KEY(p_numero_pokedex) REFERENCES Pokémon(p_numero_pokedex),
+   FOREIGN KEY(p_numero_pokedex_1) REFERENCES Pokémon(p_numero_pokedex)
 );
 
 CREATE TABLE Avoir_pour_couleur(
-   r_id INT,
-   g_num INT,
    p_numero_pokedex INT,
    c_id INT,
-   PRIMARY KEY(r_id, g_num, p_numero_pokedex, c_id),
-   FOREIGN KEY(r_id, g_num, p_numero_pokedex) REFERENCES Pokémon(r_id, g_num, p_numero_pokedex),
+   PRIMARY KEY(p_numero_pokedex, c_id),
+   FOREIGN KEY(p_numero_pokedex) REFERENCES Pokémon(p_numero_pokedex),
    FOREIGN KEY(c_id) REFERENCES Couleur(c_id)
+);
+
+CREATE TABLE Posséder(
+   p_numero_pokedex INT,
+   f_id INT,
+   PRIMARY KEY(p_numero_pokedex, f_id),
+   FOREIGN KEY(p_numero_pokedex) REFERENCES Pokémon(p_numero_pokedex),
+   FOREIGN KEY(f_id) REFERENCES Forme(f_id)
 );
 
 CREATE TABLE Se_passer(
